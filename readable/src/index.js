bef5c592aaf9213f3b45a540a098979995dea06d
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-08-31 13:58:32 -0800

    Setup Base App

--- readable/src/index.js
+++ readable/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter } from 'react-router-dom';
+import promise from 'redux-promise';
+
 import App from './App';
+import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducer)}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
