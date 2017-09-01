c1f59610408d1b59c57664507fd0312d102f561a
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-09-01 14:46:23 -0800

    Added 'Categories' to State

--- readable/src/reducers/index.js
+++ readable/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import Categories from './reducer_categories';
 import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   form: formReducer,
+  categories: Categories,
 });
 
 export default rootReducer;
