a74f0c6b791d5420ab7aad04dc88687d80bde8ba
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-09-08 10:06:59 -0800

    Added new 'reducer_posts'

--- readable/src/reducers/index.js
+++ readable/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import Categories from './reducer_categories';
+import Posts from './reducer_posts';
 import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   form: formReducer,
   categories: Categories,
+  posts: Posts,
 });
 
 export default rootReducer;
