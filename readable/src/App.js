2d3ec91b2ce6b3d65221d168bdfb2d000819d467
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-09-15 14:11:21 -0800

    Removed 'Nav' Component and 'fetchCategories' action as well.

--- readable/src/App.js
+++ readable/src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import { fetchCategories, fetchPosts } from './actions';
+import { fetchPosts } from './actions';
 
 class App extends Component {
   componentDidMount() {
